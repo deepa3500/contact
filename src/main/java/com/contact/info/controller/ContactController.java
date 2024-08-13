bf699de6f96821ba5daae7310a77e9f4863b8099
deepa3500 <73226040+deepa3500@users.noreply.github.com>
2024-08-12 23:29:34 -0400

    Update ContactController.java

--- src/main/java/com/contact/info/controller/ContactController.java
+++ src/main/java/com/contact/info/controller/ContactController.java
@@ -14,7 +14,7 @@ public class ContactController {
 		Map<String,String> cont = new HashMap<>();
 		cont.put("name", "Deepa Datta");
 		cont.put("email", "deepa9220@gmail.com");
-		System.out.println("Working as expected!!!");
+		System.out.println("Working as expected!!!Hurry!!!");
 		return cont;
 	}
 
